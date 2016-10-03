1d0c194ef6518f6d0f9c84626b50a297838862fe
HERO-PC\HERO <quanghungtruong@gmail.com>
2016-10-03 07:41:43 +0700

    upload

--- page-san-pham.php
+++ page-san-pham.php
@@ -26,4 +26,50 @@
           </a>
     </div>
 </section>
+<section class="content sp-page">
+  <div class="sp-title">Mua bàn tính</div>
+  <div class="sp-content">
+    <?php for($i=0; $i<2; $i++) : ?>
+      <div class="sp-box">
+          <div class="sp-image">
+          <a href="#">
+            <img src="<?php bloginfo('stylesheet_directory')?>/images/bigsp.jpg">
+          </a>            
+          </div>
+          <div class="sp-detail">
+            <span class="sp-name">Khung gỗ thanh mỏng</span>
+            <span class="sp-price">50 000đ</span>
+          </div>
+          <div class="btnAddCart">
+            <img src="<?php bloginfo('stylesheet_directory')?>/images/btnAddCart.png">
+          </div>
+      </div>
+    <?php endfor;?>  
+  </div>
+  <div class="sp-moi">
+    <div class="sp-title">Sản phẩm mới</div>
+      <div class="sp-content">
+        <?php for($i=0; $i<3; $i++) : ?>
+          <div class="sp-box">
+              <div class="sp-image">
+              <a href="#">
+                <img src="<?php bloginfo('stylesheet_directory')?>/images/sp.jpg">
+              </a>            
+              </div>
+              <div class="sp-detail">
+                <span class="sp-name">Khung gỗ thanh mỏng</span>
+                <span class="sp-price">50 000đ</span>
+              </div>
+              <div class="btnAddCart">
+                <img src="<?php bloginfo('stylesheet_directory')?>/images/btnAddCartNews.png">
+              </div>
+          </div>
+        <?php endfor;?>  
+      </div>
+  </div>
+  <div class="view-cart">
+      <a href="#">Xem giỏ hàng</a>
+  </div>
+</section>
+
 <?php get_footer(); ?>
